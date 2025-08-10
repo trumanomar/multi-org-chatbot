d0323cdddaa923cbe26ad83c8dec939fe183a7f3
‫youssef elzahar <49149335+youssefelzahar@users.noreply.github.com>
2025-08-10 06:29:17 +0300

    vectordb,chunking,uplodeapi,embading

--- frontend/lib/screens/login_screen.dart
+++ frontend/lib/screens/login_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import '../services/api_service.dart';
-<<<<<<< HEAD
-=======
+// ignore: unused_import
 import '../routes.dart';
->>>>>>> 6b806192b45fe7050369ec60a42d165860519246
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
