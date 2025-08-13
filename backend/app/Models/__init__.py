c11e03593b433a8c44e7e43700c35790b9adb9ca
‫youssef elzahar <49149335+youssefelzahar@users.noreply.github.com>
2025-08-13 09:13:19 +0300

    create tables,connect relationdb to vectordb,

---
+++ backend/app/Models/__init__.py
@@ -0,0 +1,2 @@
+# ده بيخلي أي حد يعمل import app.Models يقدر يوصل لكل الجداول
+from .tables import Domain, User, Docs, Chunk
--- backend/app/config.py
+++ backend/app/config.py
@@ -1,11 +1,8 @@
 import os
 
-# Make sure the Chroma DB directory is inside a persistent folder
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PERSIST_DIR = os.getenv("PERSIST_DIR", os.path.join(BASE_DIR, "..", "..", "chroma_data"))
 
-# Model for embeddings
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "sentence-transformers/all-MiniLM-L6-v2")
 
-# Ensure persistence directory exists at startup
 os.makedirs(PERSIST_DIR, exist_ok=True)
