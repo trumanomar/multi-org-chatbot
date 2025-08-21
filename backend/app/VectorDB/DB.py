27fd4d1683306ccb4f3639117fbd154ed9089b08
‫youssef elzahar <49149335+youssefelzahar@users.noreply.github.com>
2025-08-21 21:34:22 +0300

    new embedding model

--- backend/app/VectorDB/DB.py
+++ backend/app/VectorDB/DB.py
@@ -6,10 +6,10 @@ from typing import Optional, Dict, Any, List
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
-from app.config import PERSIST_DIR, MODEL_PATH
+from app.config import PERSIST_DIR, EMBEDDING_MODEL
 
 # --- Embeddings & vectorstore -------------------------------------------------
-embedding_function = HuggingFaceEmbeddings(model_name=MODEL_PATH)
+embedding_function = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
 
 # Always load existing vectorstore if it exists
 vectorstore = Chroma(
