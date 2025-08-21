a56a8e30b09e9185c47c050d80c1e5aea54843a3
‫youssef elzahar <49149335+youssefelzahar@users.noreply.github.com>
2025-08-21 16:50:33 +0300

    embedding

--- backend/app/model.py
+++ backend/app/model.py
@@ -1,9 +1,15 @@
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer,util
 
-# تحميل الموديل من HuggingFace
-model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+# # تحميل الموديل من HuggingFace
 
-# حفظه محلياً في مجلد
-model.save("./models/all-MiniLM-L6-v2")
+# model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-print("✅ تم تحميل وحفظ الموديل في ./models/all-MiniLM-L6-v2")
+# # حفظه محلياً في مجلد
+# model.save("./models/all-MiniLM-L6-v2")
+
+# print("✅ تم تحميل وحفظ الموديل في ./models/all-MiniLM-L6-v2")
+sentences = ["This is an example sentence", "Each sentence is converted"]
+
+model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+model.save("./backend/multi_models/paraphrase-multilingual-MiniLM-L12-v2")
+print("saved successfully /multi_models/paraphrase-multilingual-MiniLM-L12-v2")
